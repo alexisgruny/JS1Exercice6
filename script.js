94356344806aa6ca0ae4e66aa3901e85395d8e62
AlexisG <alexis.gruny60@gmail.com>
2018-06-20 18:25:07 +0200

    ajout regex

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function division() {
   var firstNumber = document.getElementById("firstNumber").value;
   var secondNumber = document.getElementById("secondNumber").value;
   // Recuperation des variables
-  var nombre = /\d/ ;
-  if( nombre.test(firstNumber) ||  nombre.test(secondNumber) ){
+  if(/^[0-9.]+$/.test(firstNumber && secondNumber) == true  ){
   alert(firstNumber % secondNumber);
 }
 }
