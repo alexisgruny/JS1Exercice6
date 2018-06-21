7cd0be8ef6c7d12ff397f2419774123d501e3cf0
AlexisG <alexis.gruny60@gmail.com>
2018-06-21 08:48:41 +0200

    ajout de commentaire

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function division() {
+  // Recuperation des saisies
   var firstNumber = document.getElementById("firstNumber").value;
   var secondNumber = document.getElementById("secondNumber").value;
-  // Recuperation des variables
+  // test regex
   if(/^[0-9.]+$/.test(firstNumber && secondNumber) == true  ){
+  // affichage du resultat de firstNumber et secondNumber
   alert(firstNumber % secondNumber);
 }
 }
